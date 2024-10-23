a2b628e92f0549adf5624605bafc7f4990d87ea0
Muaadh Adeleye <muaadhadeleye@gmail.com>
2024-10-23 15:37:01 +0100

    second commit

--- products/models.py
+++ products/models.py
@@ -9,12 +9,14 @@ class Product(models.Model):
         ('NGN', 'Nigerian Naira'),  # Add more currencies as needed
     ]
 
-    id = models.UUIDField( primary_key=True , default= uuid.uuid4 ,editable=False )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255, unique=True)
-    description = models.CharField(max_length=255, default='')
+    description = models.TextField(default='')  # Changed to TextField
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default='NGN')
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']  # Default ordering by name
